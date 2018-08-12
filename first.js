7ea4f9f9150c4e95364cbff0e0ab7aa801337c03
StickComs <42309149+StickComs@users.noreply.github.com>
2018-08-11 23:05:55 -0500

    Update first.js

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 function firstSite() {
-  var link = 'https://david4915.github.io/StickComs/archive/2_writing_prompt';
+  var link = 'stickcoms.com/archive/2_writing_prompt';
   location.href = link;
   return false;
 }
