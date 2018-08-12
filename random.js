b3e5d74725cdd44b32dddf1278acdd4e0052358c
StickComs <42309149+StickComs@users.noreply.github.com>
2018-08-11 23:04:38 -0500

    Update random.js

--- random.js
+++ random.js
@@ -5,7 +5,7 @@ var sites = [
 
     function randomSite() {
         var i = parseInt(Math.random() * sites.length);
-        var link = 'https://david4915.github.io/StickComs/archive/' + sites[i];
+        var link = 'stickcoms.com/archive/' + sites[i];
         location.href = link;
         return false;
     }
