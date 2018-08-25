9af7b02bbd3fa744b68bf5d2c93124996b5837ec
StickComs <42309149+StickComs@users.noreply.github.com>
2018-08-25 15:15:22 -0500

    Update first.js

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 function firstSite() {
-  var link = 'http://stickcoms.com/archive/3_love_at_first_sight';
+  var link = 'http://stickcoms.com/archive/3_dog_love';
   location.href = link;
   return false;
 }
